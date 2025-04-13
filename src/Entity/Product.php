512c0e89ed8a968a6d2ffb29ead6d9b24ec3ebbc
premganesh <ganyit@gmail.com>
2025-04-13 13:25:25 +0530

    migration changes

---
+++ src/Entity/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name): static
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+}
